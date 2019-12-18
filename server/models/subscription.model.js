61a9503aeeaee20b99ad3afe602384036d1f53bf
Virender Maira <virender@keystrokedevelopers.com>
2019-12-18 14:57:15 +0530

    create subscription model

--- server/models/subscription.model.js
+++ server/models/subscription.model.js
@@ -3,6 +3,13 @@ Schema = mongoose.Schema;
 
 const subscriptionSchema = new Schema({
     //TODO
+    signupDate: Date,
+    renewalDate: Date,
+    smsBalance: Number,
+    seriesStart: Number,
+    appId: String,
+    authId: String,
+
 });
 
 module.exports = mongoose.model('Subscription', subscriptionSchema);
