9bc38241246c2d2c4a977118cb8a5ac0d311053c
Virender Kumar <virender@keystrokedevelopers.com>
2020-03-13 22:27:00 +0530

    create sendSms

--- server/controllers/sendSms.controllers.js
+++ server/controllers/sendSms.controllers.js
@@ -1,34 +1,26 @@
-const https = require('https');
-
-
-
-
-
+const switchConnection = require('../databaseConnection/switchDb');
+const axios = require('axios');
 
 
 const sendSms = async (req, res) => {
     try {
-        let contact_num = 9466051803;
-        let show_message = 'SMS Testing';
-
-        var options = {
-            hostname: `https://www.txtguru.in/imobile/api.php?username=keystrokedeveloperscom&password=95996826&source=GYMSMS&dmobile=91${contact_num}&message=${show_message}`,
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded',
-                'Content-Length': 2
-            }
-        };
-
-        https.request(options, (res) => {
-            console.log('res-----', res);
-            console.log('statusCode:', res.statusCode);
-            console.log('headers:', res.headers);
-        }).on('error', (e) => {
-            console.log(e)
-            //  throw new Error(e.message)
-        });
-        res.status(200).send({ message: 'sms send successfully' })
+        const GymInfo = await switchConnection(req.user.newDbName, "GymInfo");
+        const SmsLog = await switchConnection(req.user.newDbName, "SmsLog");
+        const GymInfoData = await GymInfo.findOne();
+        if (GymInfoData.smsBalance >= 1) {
+            const messageResponse = await axios.post(`https://www.txtguru.in/imobile/api.php?username=keystrokedeveloperscom&password=95996826&source=GYMSMS&dmobile=91${req.body.contact}&message=${req.body.message}`);
+            const smsLogData = {};
+            smsLogData.msgId = messageResponse.data;
+            smsLogData.sendTo = req.body.contact;
+            smsLogData.smsBody = req.body.message;
+            smsLogData.smsType = 'invoice';
+            await SmsLog.create(smsLogData);
+            await GymInfo.update({ _id: GymInfoData._id }, { $inc: { smsBalance: -1 } }, { new: true });
+            res.status(200).send({ message: 'sms send successfully' })
+            return null;
+        } else {
+            throw new Error('You have not sms balance');
+        }
     } catch (err) {
         console.log('error--', err)
         res.status(400).json({ message: err.message })
--- server/models/smsLog.model.js
+++ server/models/smsLog.model.js
@@ -4,9 +4,9 @@ const moment = require('moment')
 const Schema = mongoose.Schema;
 
 const smsLogSchema = new Schema({
-    smsCount: Number,
+    smsCount: { type: Number, default: 1 },
     msgId: String,
-    sendTo: Array,
+    sendTo: String,
     smsBody: String,
     smsDate: { type: Date, default: moment(new Date()).format('YYYY-MM-DD') },
     smsType: { type: String, required: true }
